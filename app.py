b30818277685f3d54544b62fb97c17fa37dd047f
danieleduardofz <97208054+danieleduardofz@users.noreply.github.com>
2022-07-09 13:46:19 -0500

    working flask app

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 import os
-import psycopg2
-from flask import Flask, render_template
+import pandas as pd
+from sqlalchemy import create_engine
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='Home_Runs_Only',
-                            user=os.environ['postgres'],
-                            password=os.environ['admin'])
-    return conn
+    protocol = 'postgresql'
+    username = 'postgres'
+    password = 'admin'
+    host = 'localhost'
+    port = 5432
+    database_name = 'Home_Runs_Only'
+    rds_connection_string = f'{protocol}://{username}:{password}@{host}:{port}/{database_name}'
+    engine = create_engine(rds_connection_string)
+    engine.table_names()
+    return engine.connect()
 
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM sample;')
-    sampledata = cur.fetchall()
-    cur.close()
-    conn.close()
-    
-    return render_template('index.html', sampledata=sampledata) 
-
+    # pd.read_sql('select * from sample', con=conn)
+    sampledata = pd.read_sql('select * from sample', con=conn)
+    return render_template('index.html', sampledata='plow')
+    # jsonify(sampledata.to_dict()))
+    # return jsonify(sampledata.to_dict())
 if __name__ == "__main__":
     app.run(debug=True)
