51b1edad2781f9c092337413942709b9a68af459
Rashmi Rekha <rashmirekha27@gmail.com>
2022-07-13 18:37:37 -0400

    main

--- static/app.js
+++ static/app.js
@@ -1,7 +1,9 @@
 const rootUrl = "http://127.0.0.1:5000/";
-let top10AssistsPlayer;
-let top10PassesPlayer;
-let top10GoalsPlayer;
+//declare the variable to store the json response from server for different usage
+let top10AssistsPlayer; //store top 10 assists data
+let top10PassesPlayer; //store top 10 passes data
+let top10GoalsPlayer; //store top 10 goals data
+let all_data; //store all player data
 
 //filter to get array index position on id
 function findPlayerId(playerDataIds, playerId) {
@@ -11,7 +13,8 @@ function findPlayerId(playerDataIds, playerId) {
     }
   }
 }
-// function returns the json response from server
+
+// function returns the json response from server for to 10 assist
 function getTop10Assists() {
   console.log("top 10 assists");
   Plotly.d3.json(rootUrl+'/Top10Assists', function(error, response) {
@@ -20,11 +23,14 @@ function getTop10Assists() {
     } else {
       console.log("Response: " + response);
       top10AssistsPlayer = response;
-      top10assistsDropdown(top10AssistsPlayer);
+      //top10assistsDropdown(top10AssistsPlayer);
+      //drar bar chart for top 10 assists
+      plotBarChart(top10AssistsPlayer.assists,top10AssistsPlayer.name, 'Top 10 Assists');
     }
   });
 }
 
+// function returns the json response from server for to 10 passes
 function getTop10Passes() {
   console.log("top 10 passes");
   Plotly.d3.json(rootUrl+'/passes', function(error, response) {
@@ -33,24 +39,45 @@ function getTop10Passes() {
     } else {
       console.log("Response: " + response);
       top10PassesPlayer = response;
-      top10passesDropdown(top10PassesPlayer);
+      //top10passesDropdown(top10PassesPlayer);
+      //drar bar chart for top 10 passes
+      plotBarChart(top10PassesPlayer.passes_attempted,top10PassesPlayer.name,'Top 10 Passes');
     }
   });
 }
 
+// function returns the json response from server for to 10 goals
 function getTop10Goals() {
-  console.log("top 10 passes");
+  console.log("top 10 goals");
   Plotly.d3.json(rootUrl+'/goals', function(error, response) {
     if(error) {
       console.log('ERROR:: ' + error);
     } else {
       console.log("Response: " + response);
       top10GoalsPlayer = response;
-      top10goalsDropdown(top10GoalsPlayer);
+      //top10goalsDropdown(top10GoalsPlayer);
+      //drar bar chart for top 10 goals
+      plotBarChart(top10GoalsPlayer.goals,top10GoalsPlayer.name, 'Top 10 Goals');
     }
   });
 }
 
+// function returns the json response from server for all players data
+function getall_data() {
+  console.log("all_data");
+  Plotly.d3.json(rootUrl+'/all_data', function(error, response) {
+    if(error) {
+      console.log('ERROR:: ' + error);
+    } else {
+      console.log("Response: " + response);
+      all_data = response;
+      //top10assistsDropdown(top10AssistsPlayer);
+      // plotBarChart(all_dataPlayer.assists,top10AssistsPlayer.name);
+      //populate payer1 and player2 dropdown
+      populatePlayerDropdown(all_data);
+    }
+  });
+}
 
 //load  player1 and player2 drop down with player name as text and id as value
 function top10assistsDropdown(playerData) {
@@ -62,10 +89,8 @@ function top10assistsDropdown(playerData) {
   for(key in playerData.id) {
     playertable.innerHTML += '<tr> <td>'+playerData.name[key]+'</td> <td>'+playerData.assists[key]+'</td> </tr>';
     // console.log(key + '::' + playerData.name[key] + ' :: ' + playerData.assists[key]);
-    
   }
 }
-
 
 function top10passesDropdown(playerData) {
   //get the player1 dropdown id to populate with player name
@@ -75,8 +100,6 @@ function top10passesDropdown(playerData) {
   //loop through jsaon response data
   for(key in playerData.id) {
     playertable.innerHTML += '<tr> <td>'+playerData.name[key]+'</td> <td>'+playerData.passes_attempted[key]+'</td> </tr>';
-    
-    
   }
 }
 
@@ -88,8 +111,6 @@ function top10goalsDropdown(playerData) {
   //loop through jsaon response data
   for(key in playerData.id) {
     playertable.innerHTML += '<tr> <td>'+playerData.name[key]+'</td> <td>'+playerData.goals[key]+'</td> </tr>';
-    
-    
   }
 }
 
@@ -118,7 +139,6 @@ function populatePlayerDropdown(playerData) {
   }
 }
 
-
 function optionChanged(optionSelected) {
   if(optionSelected == 'Assists') {
     getTop10Assists();
@@ -129,114 +149,95 @@ function optionChanged(optionSelected) {
   }
 }
 
-
-function onChangePlayer1(playerId) {
-  let x = findPlayerId(top10AssistsPlayer.id,playerId);
-  console.log(top10AssistsPlayer.name[x]);
-  // console.log(selectedRow.assists);
-}
-//initialization
-
-getTop10Assists();
-
-
-
-
-
-
-
-
-
-
-// Select the dropdown 
-
-
-
-/*
-var selectDrop = d3.select("#selDataset");
-
-
-function init() {
-
-    //Select the dropdown 
-    var selectDrop = d3.select("#selDataset");
-
-    // Build the list that should appear in the dropdown
-    // d3.json used to access the samples.json file
-    d3.json("/").then((data) => {
-
-      // log the initial data retrieved from the samples.json file to the console
-      console.log(data);
-
-      // Create a variable that will house the data from the "name" array
-      var playerNames = data.name;
-
-      // the array.forEach() function acts like a for loop
-      // we use this action to look at each object in the "names" array
-      // and append the players 
-      playerNames.forEach((player) => {
-        selectDrop
-          .append("option")
-          .text(player)
-          .property("value", player);
-    
-     
-    });
-
-    // Build the list of datapoints that should be reflected for the first
-      // Test Subject ID No. 
-      var playerOne = playerNames[0];
-      demoData(playerOne);
-
-
-    });
-
-    d3.json("/team_stats").then((data) => {
-
-        // log the initial data retrieved from the samples.json file to the console
-        console.log(data);
-      
-       
-    });
-
-
-    d3.json("/goals").then((data) => {
-
-    // log the initial data retrieved from the samples.json file to the console
-    console.log(data);
-    
-    
-    });
-
-
-    d3.json("/passes").then((data) => {
-
-        // log the initial data retrieved from the samples.json file to the console
-        console.log(data);
-        
-        
-    });
-
-    d3.json("/all_data").then((data) => {
-
-        // log the initial data retrieved from the samples.json file to the console
-        console.log(data);
-        
-        
-    });
-
-
+//plot bar chart 
+function plotBarChart(xvalues,yvalues,chartTitle) {
+  //console.log(xvalues);
+  //console.log(yvalues);
+  //convert the values to array from json object
+  let arrayXValues = [];
+  let arrayYValues = [];
+  for(key in xvalues) {
+    arrayXValues.push(xvalues[key]);
+    arrayYValues.push(yvalues[key]);
   }
-  // Calling the init function
-  init();
+  //console.log(arrayXValues);
+  //console.log(arrayYValues);
+  
+  //draw chart
+  var trace1 = {
+    x: arrayYValues,
+    y: arrayXValues,
+    type: 'bar',
+   // text: arrayXValues,
+    orientation: 'v',
+    marker: {
+      color: 'rgba(50,171,96,0.6)',
+    }
+  };
 
-
-// optionChanged function is called in the index.html so we will go ahead and use this
-  // instead of using the on click method from javascript
-  function optionChanged(player) {
-    // Print the Test Subject ID No. that prompted the change to the console
-    console.log(player);
-
-
+  var data = [trace1];
+  var layout = {
+    title: chartTitle,
+    height: 500,
+    width: 500
+  };
+  Plotly.newPlot("bar", data, layout, {displayModeBar: false, responsive: true});
 }
-*/
+
+//function to draw comparison charts of 2 players using bar charts
+function plotComparison() {
+  //get player 1 information
+  let player1Id = document.getElementById("player1").value; //the current selected option in Player 1 drop down
+  let player1Index = findPlayerId(all_data.id,player1Id); //get the array index position of the player 1
+
+  //get player 2 information
+  let player2Id = document.getElementById("player2").value;
+  let player2Index = findPlayerId(all_data.id,player2Id); //get the array index position of the player 2
+
+  console.log('Player 1: ' + all_data.name[player1Index] + ' Assists: ' + all_data.assists[player1Index] + ' Passes: ' + all_data.passes_attempted[player1Index] + ' Goals: ' + all_data.goals[player1Index]);
+  console.log('Player 1: ' + all_data.name[player2Index] + ' Assists: ' + all_data.assists[player2Index] + ' Passes: ' + all_data.passes_attempted[player2Index] + ' Goals: ' + all_data.goals[player2Index]);
+
+  //prepare data for assists
+  var traceAssists = {
+    x: [all_data.name[player1Index], all_data.name[player2Index]],
+    y: [all_data.assists[player1Index], all_data.assists[player2Index]],
+    type: 'bar',
+    name: 'Assists'
+  };
+
+  //prepare data for passes
+  var tracePasses = {
+    x: [all_data.name[player1Index], all_data.name[player2Index]],
+    y: [all_data.passes_attempted[player1Index], all_data.passes_attempted[player2Index]],
+    type: 'bar',
+    name: 'Passes'
+  };
+
+  //prepare data for goals
+  var traceGoals = {
+    x: [all_data.name[player1Index], all_data.name[player2Index]],
+    y: [all_data.goals[player1Index], all_data.goals[player2Index]],
+    type: 'bar',
+    name: 'Goals'
+  };
+
+  var data = [traceGoals, traceAssists]; //, tracePasses
+
+  var layout = {
+      title: all_data.name[player1Index] + ' vs ' + all_data.name[player2Index],
+      showlegend: true
+  };
+
+  Plotly.newPlot('comparison', data, layout, {displayModeBar: false, responsive: true});
+}
+
+//initialization code
+getTop10Assists();
+getall_data();
+
+
+
+
+
+
+
